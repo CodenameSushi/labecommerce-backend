bf50472e4a3e8014c91e89a86a987fb8a026e774
CodenameSushi <fhenrique08@gmail.com>
2023-01-12 18:15:07 -0300

    Exercicios

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ import {
   getAllPurchasesFromUserId,
   getAllPurchases
 } from "./database";
-import { TUser, TProduct, TPurchase } from "./types";
+import { TUser, TProduct, TPurchase, PRODUCT_TYPE } from "./types";
 import express, {Request, Response} from "express";
 import cors from 'cors';
 
@@ -28,6 +28,14 @@ app.get("/users", (req:Request, res:Response) => {
   res.status(200).send(getAllUsers())
 })
 
+app.get("/users/:id/purchases", (req:Request, res:Response) => {
+  const id = req.params.id
+  const result = purchases.filter((purchase) => {
+    return purchase.userId === id
+  })
+  res.status(200).send(result)
+})
+
 app.get("/products", (req:Request, res:Response) => {
   res.status(200).send(getAllProducts())
 })
@@ -40,9 +48,19 @@ app.get("/products/search", (req:Request, res:Response) => {
   res.status(200).send(queryProductsByName(q))
 })
 
+app.get("/products/:id", (req:Request, res:Response) => {
+  const id = req.params.id
+  const result = products.find((product) => {
+    return product.id === id
+  }) 
+res.status(200).send(result)
+
+})
+
 app.get("/purchases", (req:Request, res:Response) => {
   res.status(200).send(getAllPurchases())
 })
+
 
 app.post("/users", (req:Request, res:Response) => {
   const {id, email, password} = req.body as TUser
@@ -60,6 +78,71 @@ app.post("/purchases", (req:Request, res:Response) => {
   const {userId, productId, quantity, totalPrice} = req.body as TPurchase
   createPurchase(userId, productId, quantity, totalPrice)
   res.status(201).send("Compra realizada com sucesso!")
+})
+
+app.delete("/users/:id", (req:Request, res:Response) => {
+  const id = req.params.id
+  const userIndex = users.findIndex((user) => {
+    return user.id === id
+  })
+  if(userIndex >= 0){
+    users.splice(userIndex,1)
+    res.status(200).send("Usuario deletado com sucesso!")
+  }else{
+    res.status(404).send("Usuario nao encontrado!")
+  }
+})
+
+app.delete("/products/:id", (req:Request, res:Response) => {
+  const id = req.params.id
+  const productIndex = products.findIndex((product)=>{
+    return product.id === id
+  })
+  if(productIndex >= 0){
+    products.splice(productIndex,1)
+    res.status(200).send("Produto apagado com sucesso!")
+  }else{
+    res.status(404).send("Produto nao encontrado!")
+  }
+})
+
+app.put("/users/:id", (req:Request, res:Response) => {
+  const id = req.params.id
+
+  const newEmail = req.body.email as string | undefined
+  const newPassword = req.body.password as string | undefined
+
+  const user = users.find((user) => {
+    return user.id === id
+  })
+
+  if(user){
+    user.email = newEmail || user.email
+    user.password = newPassword || user.password
+    res.status(200).send("Usuario editado com sucesso!")
+  }else{
+    res.status(404).send("Usuario nao encontrado")
+  }
+
+})
+
+app.put("/products/:id", (req:Request, res:Response) => {
+  const id = req.params.id
+  const newName = req.body.name as string | undefined
+  const newPrice = req.body.price as number | undefined
+  const newCategory = req.body.category as PRODUCT_TYPE | undefined
+
+  const product = products.find((product) => {
+    return product.id === id
+  })
+  if(product){
+    product.name = newName || product.name
+    product.price = newPrice || product.price
+    product.category = newCategory || product.category
+    res.status(200).send("Produto editado com sucesso!")
+  }else{
+    res.status(404).send("Produto nao encontrado!")
+  }
 })
 
 
